4b4516849bf983af004bd1bc48769bf746265184
Hafiz Atif Salfi <salfiatif560@gmail.com>
2025-08-29 20:57:03 -0700

    Added a new image file and updated page.js

--- app/page.js
+++ app/page.js
@@ -60,7 +60,7 @@ const Portfolio = () => {
             <header className="hero">
                 <div className="hero-content">
                     <Image
-                            src="/BG2.png" 
+                            src="/Bg1.png" 
                             alt="Your Profile Picture"
                             className="profile-pic"
                             width={200}
